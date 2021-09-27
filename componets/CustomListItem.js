abd8149adeca3c6626ecdd083fe3943d911f0382
Manglesh__ <manglesh.tiwari@oodles.io>
2021-09-28 03:15:18 +0530

    added all the functionalities and wrapped up

--- componets/CustomListItem.js
+++ componets/CustomListItem.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements'
+import { db } from '../firebase';
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+
+    const [chatMessages, setChatMessages] = useState([])
+
+    useEffect(() => {
+        const unsubscribe = db
+            .collection('chats')
+            .doc(id)
+            .collection('messages')
+            .orderBy('timeStamp', 'desc')
+            .onSnapshot((snapshot) =>
+                setChatMessages(snapshot.docs.map(doc => doc.data()))
+            )
+    })
+
+
     return (
         <ListItem
             key={id}
             bottomDivider
             style={styles.list}
-            onPress={() => enterChat(id, chatName)} >
+            onPress={() => enterChat(id, chatName)}
+        >
             <Avatar
                 rounded
                 source={{
-                    uri: "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+                    uri:
+                        chatMessages?.[0]?.photoURL || "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
                 }} />
             <ListItem.Content>
                 <ListItem.Title style={{ fontWeight: "800" }}>
@@ -21,7 +39,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
                 <ListItem.Subtitle
                     numberOfLines={1}
                     ellipsizeMode='tail'>
-                    ABC
+                    {chatMessages?.[0]?.displayName}:{chatMessages?.[0]?.message}
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
